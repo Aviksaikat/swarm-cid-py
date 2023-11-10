abc3e8d99184a7503d06e0fa09b5ce24c1d49421
SAIKAT KARMAKAR <saikickkarma@gmail.com>
2023-11-10 01:34:33 +0000

    Nov 9 update. setup.py updated to fix workflow

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ extras_require = {
     ],
     "lint": [
         "black>=23.11.0,<24",  # Auto-formatter and linter
-        "isort>=5.12.0,<6",
+        "isort>=5.12.0,<6",  # Import sorting linter
         "mypy>=1.5.1,<2",  # Static type analyzer
         "types-PyYAML",  # Needed due to mypy typeshed
         "types-requests",  # Needed due to mypy typeshed
@@ -17,7 +17,6 @@ extras_require = {
         "flake8>=6.1.0,<7",  # Style linter
         "flake8-breakpoint>=1.1.0,<2",  # detect breakpoints left in code
         "flake8-print>=4.0.1,<5",  # detect print statements left in code
-        "isort>=5.10.1,<6",  # Import sorting linter
         "mdformat>=0.7.17",  # Auto-formatter for markdown
         "mdformat-gfm>=0.3.5",  # Needed for formatting GitHub-flavored markdown
         "mdformat-frontmatter>=0.4.1",  # Needed for frontmatters-style
